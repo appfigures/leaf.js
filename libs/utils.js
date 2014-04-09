707ac89978d33fa6515f7e5c4a436783d2fc981b
Oz <oz@pixelwave.org>
2014-04-09 11:45:28 -0400

    Cleaned up unused code

--- libs/utils.js
+++ libs/utils.js
@@ -6,25 +6,6 @@ var globals = require('./globals'),
     utils;
 
 utils = {
-	// forEach: function (obj, fn) {
- //        var key;
- //        if (obj == null || !fn) return;
-
- //        if (utils.isArrayLike(obj)) {
- //            for (key = 0; key < obj.length; ++key) {
- //                fn(obj[key], key);
- //            }
- //        } else {
- //            for (key in obj) {
- //                if (obj.hasOwnProperty(key)) {
- //                    fn(obj[key], key);
- //                }
- //            }
- //        }
- //    },
-    // map: function (obj, fn) {
-    //     return Array.prototype.map.call(obj, fn);
-    // },
     // My very rough version
     isArrayLike: function (obj) {
         return (obj instanceof Array) || (typeof obj !== 'string' && typeof obj.length === 'number');
@@ -39,21 +20,10 @@ utils = {
     isNumeric: function (str) {
         return !isNaN(parseFloat(str)) && isFinite(str);
     },
-    // isFunction: function (value) {
-    //     var getType = {};
-    //     return value && getType.toString.call(value) === '[object Function]';
-    // },
     toCamelCase: function (string) {
         return string.replace(toCamelCaseRegexp, function($1){return $1.toUpperCase().replace('-','');});
     },
-    // extend: function (dst) {
-    //     utils.forEach(Array.prototype.slice.call(arguments, 1), function (src) {
-    //         var key;
-    //         if (src) for (key in src) dst[key] = src[key];
-    //     });
-    //     return dst;
-    // },
-    
+    // TODO: Accept a cache
     loadFile: function (path) {
         var fileCache = cache.$get('file');
         if (fileCache[path]) {
@@ -71,32 +41,7 @@ utils = {
         content = content.toString();
         fileCache[path] = content;
         return content;
-    },
-
-    // TODO: These aren't used by leaf, just the modules and renderer
-    // getBasePath: function (path) {
-    //     var index = path.lastIndexOf('/');
-
-    //     if (index < 0) return '';
-
-    //     // File sitting in the root
-    //     if (index === 0) return '/';
-
-    //     // If the last '/' isn't in the middle, it's a dir
-    //     if (index === path.length - 1) return path;
-
-    //     return path.substring(0, index);
-    // },
-
-    // Does nothing if the path is absolute
-    // setBasePath: function (path, base) {
-    //     var delimiter = '/';
-    //     // It's an absolute path already
-    //     if (path.charAt(0) === delimiter) return path;
-    //     if (base.charAt(base.length - 1) !== delimiter) base += delimiter;
-
-    //     return base + path;
-    // }
+    }
 };
 
 module.exports = utils;
